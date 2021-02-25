2b9c59ecb7711e19ccc6e0d92996698bdce1dfef
Suji <42132416+sujikathir@users.noreply.github.com>
2021-02-25 15:54:39 +0530

    Update Opensource

--- src/pages/opensource/Opensource.js
+++ src/pages/opensource/Opensource.js
@@ -1,4 +1,4 @@
-/*import React, { Component } from "react";
+import React, { Component } from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import OpensourceCharts from "../../containers/opensourceCharts/OpensourceCharts";
@@ -25,4 +25,3 @@ class Opensource extends Component {
 }
 
 export default Opensource;
-*/
